976918ca6c3d5115cab41d9d5a0bfd129d79bb04
BernardAli <alibernard.coding@gmail.com>
2022-09-18 10:57:07 +0000

    Added Post Images

--- social/forms.py
+++ social/forms.py
@@ -8,12 +8,13 @@ class PostForm(forms.ModelForm):
         widget=forms.Textarea(attrs={
             'rows': '3',
             'placeholder': 'Say Something...'
-        })
-    )
+            }))
+
+    image = forms.ImageField(required=False)
 
     class Meta:
         model = Post
-        fields = ["body"]
+        fields = ['body', 'image']
 
 
 class CommentForm(forms.ModelForm):
